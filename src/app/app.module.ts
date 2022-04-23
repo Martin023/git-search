a609993b661fd59c346884df72eb4f90b3af8d3a
martin023 <mainamartin023@gmail.com>
2022-04-23 18:44:27 +0300

    This commit will add display for user results, and search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SearchUserComponent } from './search-user/search-user.component';
+import { SearchRepoComponent } from './search-repo/search-repo.component';
+import { DisplayResultsComponent } from './display-results/display-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    SearchUserComponent,
+    SearchRepoComponent,
+    DisplayResultsComponent
   ],
   imports: [
     BrowserModule,
