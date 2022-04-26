496996f6e7fafb9901de338e0c2164e9059b1ffe
martin023 <mainamartin023@gmail.com>
2022-04-26 18:42:49 +0300

    This commit will add get repositories function to return repositories

--- src/app/fetch-data.service.ts
+++ src/app/fetch-data.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment'
 import { HttpClient} from '@angular/common/http';
+import { throwError,catchError } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +13,16 @@ export class FetchDataService {
   
   getUser(): Promise<any> {
     return this.http.get(`${environment.apiUrl}${this.userName}??Authorization=+${environment.apiKey}`)
+    .pipe(
+      catchError(err=>{
+        console.log(err);
+        return throwError(
+          'Something bad happened; please try again later.');
+      })
+    ).toPromise();
+  }
+  getRepositories(): Promise<any> {
+    return this.http.get(`${environment.apiUrl}${this.userName}/repos??Authorization=+${environment.apiKey}`)
     .pipe(
       catchError(err=>{
         console.log(err);
