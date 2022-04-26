b768dda66669b06027a36072c068d6b7b4bfb87c
martin023 <mainamartin023@gmail.com>
2022-04-26 19:26:19 +0300

    This commit will add repository search logic

--- src/app/repository-search/repository-search.component.ts
+++ src/app/repository-search/repository-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Repository } from '../repository'
+import { FetchDataService } from '../fetch-data.service';
+
 
 @Component({
   selector: 'app-repository-search',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repository-search.component.css']
 })
 export class RepositorySearchComponent implements OnInit {
+  searchResult!: Repository
+  repositories:Repository[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  githubsearch!: FetchDataService;
+  searchText: any;
+  constructor(githubsearch: FetchDataService) { 
+    this.githubsearch = githubsearch;
   }
 
+  ngOnInit(): void {
+    this.githubsearch.getRepositories().then((repositories) => {
+      this.repositories = repositories
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
+searchRepositories(): void {
+  console.log(this.searchText);
+  this.githubsearch
+    .searchRepositories(this.searchText)
+    .then((searchResult) => {
+      console.log(searchResult);
+      this.repositories = searchResult
+         
+})
+}
+
+}
+  
+
+
