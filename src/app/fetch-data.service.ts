646db865a9846543446e7c818c62401b4b8114b9
martin023 <mainamartin023@gmail.com>
2022-04-26 18:40:42 +0300

    This commit will add get user function

--- src/app/fetch-data.service.ts
+++ src/app/fetch-data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from 'src/environments/environment'
+import { HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class FetchDataService {
-
-  constructor() { }
-}
+  private userName!:string;
+  constructor(private http:HttpClient) { }
+  
+  getUser(): Promise<any> {
+    return this.http.get(`${environment.apiUrl}${this.userName}??Authorization=+${environment.apiKey}`)
+    .pipe(
+      catchError(err=>{
+        console.log(err);
+        return throwError(
+          'Something bad happened; please try again later.');
+      })
+    ).toPromise();
+  }
