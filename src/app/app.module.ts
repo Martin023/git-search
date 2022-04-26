61f79ad1109f591786e34cce383233ae131388f1
martin023 <mainamartin023@gmail.com>
2022-04-26 19:19:11 +0300

    This commit will add repository search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { FormsModule } from '@angular/forms';
 import { FetchDataService } from './fetch-data.service';
 import { UserComponent } from './user/user.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RepositorySearchComponent } from './repository-search/repository-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    NavBarComponent
+    NavBarComponent,
+    RepositorySearchComponent
   ],
   imports: [
     BrowserModule,
